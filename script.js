ce9d7a263435ffbf0cf1c53286cdb24a545c1469
Hamza Anwar <hamzaanwardev@gmail.com>
2021-09-03 22:08:40 +0500

    JavaScript Done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const toggle_btn = document.querySelectorAll('.faq-toggle');
-toggle_btn.forEach(toggle_btn => {
-    toggle_btn.addEventListener('click', () => {
-        
+toggle_btn.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
     })
 });
